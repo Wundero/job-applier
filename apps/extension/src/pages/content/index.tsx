3063b3c70f06af07229d51226abb147ac06e3b83
sam <sublimecraftftw@gmail.com>
2024-05-09 04:57:44 -0700

    Fonts work

--- apps/extension/src/pages/content/index.tsx
+++ apps/extension/src/pages/content/index.tsx
@@ -1,20 +1,22 @@
-import { createRoot } from 'react-dom/client';
-import './style.css' 
-// const div = document.createElement('div');
-// div.id = '__root';
+import { createRoot } from "react-dom/client";
+
+import "./style.css";
+
+// const div = document.createElement("div");
+// div.id = "__root__applier";
 // document.body.appendChild(div);
 
-// const rootContainer = document.querySelector('#__root');
+// const rootContainer = document.querySelector("#__root__applier");
 // if (!rootContainer) throw new Error("Can't find Options root element");
 // const root = createRoot(rootContainer);
 // root.render(
-//   <div className='absolute bottom-0 left-0 text-lg text-black bg-amber-400 z-50'  >
-//     content script loaded
-//   </div>
+//   <div className="font-sans absolute bottom-0 left-0 z-50 bg-amber-400 text-lg text-black">
+//     content script loaded - applier
+//   </div>,
 // );
 
 try {
-  console.log('content script loaded');
+  console.log("content script loaded");
 } catch (e) {
   console.error(e);
 }
